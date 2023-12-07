442bed054f99d5405655eb0987e9859c91630a99
Kcirevam16800 <153153964+Kcirevam16800@users.noreply.github.com>
2023-12-07 10:47:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+//2372056 Maverick Rafael Tanadi C
+document.getElementById('kelas').addEventListener('change', function() {
+    let pilihkelas = this.value;
+    let fotokelas = document.getElementById('Foto');
+
+    if (pilihkelas === 'A') {
+        fotokelas.src = 'images/a.jpg';
+    } else if (pilihkelas === 'B') {
+        fotokelas.src = 'images/b.jpg';
+    } else if (pilihkelas === 'C') {
+        fotokelas.src = 'images/c.jpg';
+    } else if (pilihkelas === '') {
+        alert('Ayo pilih dulu kelas favouritenya donk!');
+    };
+});
